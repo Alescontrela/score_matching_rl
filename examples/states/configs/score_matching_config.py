ee7248758a97614a3143b248f8d2049eba2c7b5b
michael-psenka <michael.psenka@gmail.com>
2024-06-07 16:45:13 -0700

    add config options for actor/critic dims and score matching scaling

--- examples/states/configs/score_matching_config.py
+++ examples/states/configs/score_matching_config.py
@@ -1,6 +1,5 @@
 import ml_collections
 
-
 def get_config():
     config = ml_collections.ConfigDict()
     config.model_cls = "ScoreMatchingLearner"
@@ -8,5 +7,9 @@ def get_config():
     config.critic_lr = 3e-4
     config.discount = 0.99
     config.tau = 0.005  # For soft target updates.
-    config.T = 15
+    config.T = 5
+    config.M_q = 50
+    config.ddpm_temperature=0.2
+    config.critic_hidden_dims=(512,512)
+    config.actor_hidden_dims=(512,512)
     return config
