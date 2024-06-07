9d7e7c5ca96afd0207c07dac6c64ee4ff31a17d6
michael-psenka <michael.psenka@gmail.com>
2024-06-07 16:44:01 -0700

    update structure and features to matching ddpm_online train scripts

--- examples/states/train_online.py
+++ examples/states/train_online.py
@@ -1,12 +1,27 @@
+
 #! /usr/bin/env python
-import dmcgym
+# import dmcgym
+import jax.profiler
+import jax
+# timer
+import time
+
 import gym
 import tqdm
 import wandb
 from absl import app, flags
 from ml_collections import config_flags
+import numpy as np
 
-from jaxrl5.agents import SACLearner, TD3Learner
+import time
+import dmc2gym
+
+import os
+os.environ['MKL_SERVICE_FORCE_INTEL'] = '1'
+os.environ['MUJOCO_GL'] = 'egl'
+
+
+from jaxrl5.agents import TD3Learner, SACLearner
 from jaxrl5.data import ReplayBuffer
 from jaxrl5.evaluation import evaluate
 from jaxrl5.wrappers import wrap_gym
@@ -15,18 +30,20 @@ from jaxrl5.wrappers.wandb_video import WANDBVideo
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string("project_name", "jaxrl5_online", "wandb project name.")
-flags.DEFINE_string("env_name", "HalfCheetah-v4", "Environment name.")
+flags.DEFINE_string("run_name", "", "wandb run name.")
+flags.DEFINE_string("env_name", "cartpole_balance", "Environment name.")
 flags.DEFINE_integer("seed", 42, "Random seed.")
-flags.DEFINE_integer("eval_episodes", 10, "Number of episodes used for evaluation.")
+flags.DEFINE_integer("eval_episodes", 1, "Number of episodes used for evaluation.")
 flags.DEFINE_integer("log_interval", 1000, "Logging interval.")
-flags.DEFINE_integer("eval_interval", 5000, "Eval interval.")
+flags.DEFINE_integer("eval_interval", 10000, "Eval interval.")
 flags.DEFINE_integer("batch_size", 256, "Mini batch size.")
 flags.DEFINE_integer("max_steps", int(1e6), "Number of training steps.")
 flags.DEFINE_integer(
     "start_training", int(1e4), "Number of training steps to start training."
 )
 flags.DEFINE_boolean("tqdm", True, "Use tqdm progress bar.")
-flags.DEFINE_boolean("save_video", False, "Save videos during evaluation.")
+flags.DEFINE_boolean("wandb", False, "Use wandb")
+flags.DEFINE_boolean("save_video", True, "Save videos during evaluation.")
 flags.DEFINE_integer("utd_ratio", 1, "Update to data ratio.")
 config_flags.DEFINE_config_file(
     "config",
@@ -37,22 +54,34 @@ config_flags.DEFINE_config_file(
 
 
 def main(_):
-    wandb.init(project=FLAGS.project_name)
-    wandb.config.update(FLAGS)
+    if FLAGS.wandb:
+        if FLAGS.run_name != "":
+            wandb.init(project=FLAGS.project_name, name=FLAGS.run_name, tags=[FLAGS.run_name])
+        else:
+            wandb.init(project=FLAGS.project_name)
+        wandb.config.update(FLAGS)
 
-    env = gym.make(FLAGS.env_name)
+    suite, task = FLAGS.env_name.split('_')
+    env = dmc2gym.make(domain_name=suite, task_name=task, seed=1)
+    # env = gym.make(FLAGS.env_name)
     env = wrap_gym(env, rescale_actions=True)
     env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=1)
-    if FLAGS.save_video:
+    if FLAGS.wandb and FLAGS.save_video:
         env = WANDBVideo(env)
     env.seed(FLAGS.seed)
 
-    eval_env = gym.make(FLAGS.env_name)
+    print('Evironment info:')
+    print(env.observation_space)
+    print(env.action_space)
+
+    eval_env = dmc2gym.make(domain_name=suite, task_name=task, seed=1)
+    # eval_env = gym.make(FLAGS.env_name)
     eval_env = wrap_gym(eval_env, rescale_actions=True)
     eval_env.seed(FLAGS.seed + 42)
 
     kwargs = dict(FLAGS.config)
     model_cls = kwargs.pop("model_cls")
+
     agent = globals()[model_cls].create(
         FLAGS.seed, env.observation_space, env.action_space, **kwargs
     )
@@ -63,6 +92,7 @@ def main(_):
     replay_buffer.seed(FLAGS.seed)
 
     observation, done = env.reset(), False
+
     for i in tqdm.tqdm(
         range(1, FLAGS.max_steps + 1), smoothing=0.1, disable=not FLAGS.tqdm
     ):
@@ -70,6 +100,7 @@ def main(_):
             action = env.action_space.sample()
         else:
             action, agent = agent.sample_actions(observation)
+            action = np.asarray(action)
         next_observation, reward, done, info = env.step(action)
 
         if not done or "TimeLimit.truncated" in info:
@@ -91,19 +122,21 @@ def main(_):
 
         if done:
             observation, done = env.reset(), False
-            for k, v in info["episode"].items():
-                decode = {"r": "return", "l": "length", "t": "time"}
-                wandb.log({f"training/{decode[k]}": v}, step=i)
+            if FLAGS.wandb:
+                for k, v in info["episode"].items():
+                    decode = {"r": "return", "l": "length", "t": "time"}
+                    wandb.log({f"training/{decode[k]}": v}, step=i)
 
         if i >= FLAGS.start_training:
             batch = replay_buffer.sample(FLAGS.batch_size * FLAGS.utd_ratio)
             agent, update_info = agent.update(batch, FLAGS.utd_ratio)
 
-            if i % FLAGS.log_interval == 0:
+
+            if FLAGS.wandb and i % FLAGS.log_interval == 0:
                 for k, v in update_info.items():
                     wandb.log({f"training/{k}": v}, step=i)
 
-        if i % FLAGS.eval_interval == 0:
+        if FLAGS.wandb and i % FLAGS.eval_interval == 0:
             eval_info = evaluate(
                 agent,
                 eval_env,
@@ -112,7 +145,6 @@ def main(_):
             )
             for k, v in eval_info.items():
                 wandb.log({f"evaluation/{k}": v}, step=i)
-
 
 if __name__ == "__main__":
     app.run(main)
